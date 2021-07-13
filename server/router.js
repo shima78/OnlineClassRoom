7129dcd91a2a6edff67c70db14f599bbfd1e755d
ShotgunkeeperVZ <55543338+ShotgunkeeperVZ@users.noreply.github.com>
2021-07-14 02:43:11 +0430

    Create router.js
    fekr mikonm fght chanta console log ezaf dare but anyway..

---
+++ server/router.js
@@ -0,0 +1,38 @@
+var express = require("express");
+var router = express.Router();
+const jwt = require('jsonwebtoken');
+const userDB = require('./db/user'); 
+var multer  = require('multer');
+
+
+router.post('/login', multer().none(), async(req, res) => {   
+    
+    console.log("connected");
+    console.log(req.body);
+    const username = req.body["username"]
+    const password = req.body["password"] 
+
+    const result = await userDB.getUser(username,password)
+    console.log(result)
+    if(result.length){
+        const usr = result[0]
+        console.log(usr)
+        jwt.sign({usr}, 'secretkey', { expiresIn: '2h' }, (err, token) => {
+            res.json({
+            // user : user,
+            token : token
+            });
+        });
+        
+    }
+    else{
+        res.status(400).send({
+            message: 'This is an error!'
+         });
+         
+    }
+
+
+}); 
+
+module.exports = router;
